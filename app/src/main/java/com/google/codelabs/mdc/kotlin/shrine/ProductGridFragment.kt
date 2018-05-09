d4d9d02c1893c7dc5f9430bb2e09499361fee5df
tanmay <tanmaysingal@icloud.com>
2018-05-09 09:30:30 +0530

    Grid View with images

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -3,7 +3,9 @@ package com.google.codelabs.mdc.kotlin.shrine
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.view.*
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
@@ -21,6 +23,15 @@ class ProductGridFragment : Fragment() {
 
         // Setting the toolbar
         (activity as AppCompatActivity).setSupportActionBar(v.app_bar)
+
+        // Set-up the recycler view
+        v.recycler_view.setHasFixedSize(true)
+        v.recycler_view.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+        val adapter = ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+        v.recycler_view.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+        v.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
 
         return v
     }
